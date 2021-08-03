3eb1ac404ba4d3ea6772352d0272b2f51dd5be7c
Harold Rodriguez <harol20082008@hotmail.com>
2021-08-03 11:02:34 -0300

    beginning_front_end

---
+++ client/src/Redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../Reducers/index";
+import thunk from "redux-thunk";
+
+const store = createStore( rootReducer,
+                           window._REDUX_DEVTOOLS_EXTENSION__ && window._REDUX_DEVTOOLS_EXTENSION__(),
+                           applyMiddleware(thunk) 
+);
+
+export default store;
