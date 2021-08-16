8b0341d0f83fe67482e57666988b796101e7bcd6
Harold Rodriguez <harol20082008@hotmail.com>
2021-08-16 16:36:32 -0500

    1608_ahivoy

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import { getAllPokemons,getTypes,sortByAttack, sortAlphabetically } from '../../Redux/action';
+import { getAllPokemons,getTypes,sortByAttack, sortAlphabetically, filterByPokemons,filterByType } from '../../Redux/action';
 import Pokemons from '../Pokemons/Pokemons';
-import Filter from '../Filter/Filter';
 import Paged from '../Paged/Paged';
 import home from './home.module.css'
 
@@ -44,19 +43,50 @@ export default function Home(){
         setSort(`Sorted ${e.target.value}`);
     }
 
+    function handleFilterByType(e){
+        dispatch(filterByType(e.target.value));
+    }
+
+    function handleFilterPokemons(e){
+        dispatch(filterByPokemons(e.target.value));
+    }
+
     return (
-    <div className={home.home}>
-        <Filter types={types} />
-        <span>Sort by attack:</span>
-        <select onChange={(e) => handleSortByAttack(e)}> 
-            <option value="desc">Higher to lower</option>
-            <option value="asc">Lower to higher</option>
-        </select>
-        <span>Sort alphabetically:</span>
-        <select onChange={handleSortByAlphabetically}> 
-            <option value="asc_name">A-Z</option>
-            <option value="desc_name">Z-A</option>
-        </select>
+    <div >
+        <div className={home.filterSort}>
+            <div>
+                <span>Pokemons:</span>
+                <select onChange={handleFilterPokemons} className={home.select}>
+                    <option value="all" className={home.select}>All</option>
+                    <option value="create" className={home.select}>Created</option>
+                    <option value="from_api" className={home.select}>From API</option>
+                </select>
+            </div>
+            <div>
+                <span>Filter by type:</span>
+                <select onChange={(e) => handleFilterByType(e)} className={home.select}>
+                    {
+                        types?.map((type,i) => (
+                            <option value={type.name.toLowerCase()} key ={i}>{type.name.toUpperCase()}</option>
+                            ))
+                        }
+                </select>
+            </div>
+            <div>
+                <span>Sort by attack:</span>
+                <select onChange={(e) => handleSortByAttack(e)} className={home.select}> 
+                    <option value="desc">Higher to lower</option>
+                    <option value="asc">Lower to higher</option>
+                </select>
+            </div>
+            <div>
+                <span>Sort alphabetically:</span>
+                <select onChange={handleSortByAlphabetically} className={home.select}> 
+                    <option value="asc_name">A-Z</option>
+                    <option value="desc_name">Z-A</option>
+                </select>
+            </div>
+        </div>
         <Pokemons pokemons={currentPokemons}/>
         <Paged
          pokemons={pokemons.length}
