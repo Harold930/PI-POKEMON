f74b00bac34c17332c660fa4823d29eee4b2bf16
Harold Rodriguez <harol20082008@hotmail.com>
2021-08-17 13:54:12 -0500

    1708_component_detail

--- client/src/Components/Detail/Detail.jsx
+++ client/src/Components/Detail/Detail.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { getPokemonById, getPokemonByName } from '../../Redux/action';
-import putinzard from '../../img/putinzard.jpg'
+import putinzard from '../../img/putinzard.jpg';
+import detail from './detail.module.css';
 
 export default function Detail({match}){
     console.log(match.params.id);
@@ -21,38 +22,43 @@ export default function Detail({match}){
     console.log(pokemonById)
     if(pokemonById.msg !== 'Pokemon dont found'){
     return(
-            <div>
-                <h2>{pokemonById.name}</h2>
-                <h3>#{pokemonById.id}</h3>
-                <img src={pokemonById.sprite ? pokemonById.sprite : `${putinzard}`} width="80" height="100" alt='Dont found' />
-               <div>
-                    {
-                        pokemonById.types && pokemonById.types.map((pokemon,i) => (
-                            <li key = {i}>{pokemon}</li>
-                        ))
-                    }
+        <div className={detail.contain}>
+            <div className={detail.detail}>
+                    <h2 className={detail.name_id}>{pokemonById.name}</h2>
+                    <h3 className={detail.name_id}>#{pokemonById.id}</h3>
+                <div className={detail.img_types}>
+                            <div className={detail.types}>
+                                {
+                                    pokemonById.types && pokemonById.types.map((pokemon,i) => (
+                                        <li key = {i}>{pokemon}</li>
+                                        ))
+                                    }
+                                </div>
+                            
+                            <img src={pokemonById.sprite ? pokemonById.sprite : `${putinzard}`} width="180" height="150" alt='Dont found' />
+                    </div>
+                    <div className={detail.stats}>
+                            <span>
+                                Hp: {pokemonById.hp}
+                            </span>
+                            <span>
+                                Attack: {pokemonById.attack}
+                            </span>
+                            <span>
+                                Defense: {pokemonById.defense}
+                            </span>
+                            <span>
+                                Speed: {pokemonById.speed}
+                            </span>
+                            <span>
+                                Height: {pokemonById.height}
+                            </span>
+                            <span>
+                                Weight: {pokemonById.weight}
+                            </span>
+                    </div>
                 </div>
-               <div>  
-                   <span>
-                       Hp: {pokemonById.hp}
-                   </span>
-                   <span>
-                       Attack: {pokemonById.attack}
-                   </span>
-                   <span>
-                       Defense: {pokemonById.defense}
-                   </span>
-                   <span>
-                       Speed: {pokemonById.speed}
-                   </span>
-                   <span>
-                       Height: {pokemonById.height}
-                   </span>
-                   <span>
-                       Weight: {pokemonById.weight}
-                   </span>
-                 
-               </div>
+    
             </div>
     )
 }   else {
